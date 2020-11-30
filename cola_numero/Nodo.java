27c5497a0e5dc315c929c0ed401fd1cbff3f3673
AngelMelendez2020 <71795575+AngelMelendez2020@users.noreply.github.com>
2020-11-29 22:25:08 -0600

    Create Nodo.java

---
+++ cola_numero/Nodo.java
@@ -0,0 +1,12 @@
+package cola_numero;
+
+public class Nodo {
+   String nombre;
+    int num;
+    Nodo siguiente;
+    public Nodo(String nombre,int num){
+        this.nombre=nombre;
+        this.num=num;
+        this.siguiente=null;
+    }
+}
